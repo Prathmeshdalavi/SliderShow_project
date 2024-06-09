03ef865f68ef9f6dbe299c7cb6b294bbd5b9fa58
Prathmesh Dalavi <prathmeshdalavi195@gmail.com>
2024-06-09 10:50:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+  let nextDOm = document.getElementById('next');
+  let prevDom = document.getElementById('prev');
+  let carouselDom = document.querySelector('.carousel');
+  let listItemDom = document.querySelector('.carousel .list');
+  let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+  nextDOm.onclick = function(){
+    showSlider('next');
+
+  }
+  prevDom.onclick = function(){
+    showSlider('prev');
+  }
+  let timeRunning = 3000;
+  let timeAutoNext = 5000;
+  let runTimeOut;
+  let runAutoRun = setTimeout(() =>{
+    nextDOm.click();
+   },timeAutoNext);
+   
+  function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+ 
+    if(type == 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next');
+    }else{
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        carouselDom.classList.add('prev');
+
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() =>{
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    },timeRunning);
+
+
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(() =>{
+     nextDOm.click();
+    },timeAutoNext);
+  }
+
